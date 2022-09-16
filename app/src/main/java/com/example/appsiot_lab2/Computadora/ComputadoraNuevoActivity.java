f520b76e0d4bff911297eaacb9c9a88c2c8bbe0b
Alessandra Morales <azmoralesm@pucp.edu.pe>
2022-09-16 08:28:06 -0500

    Carpetas

---
+++ app/src/main/java/com/example/appsiot_lab2/Computadora/ComputadoraNuevoActivity.java
@@ -0,0 +1,14 @@
+package com.example.appsiot_lab2.Computadora;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ComputadoraNuevoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_computadora_nuevo);
+    }
+}
---
+++ app/src/main/java/com/example/appsiot_lab2/Teclado/TecladoNuevoActivity.java
@@ -0,0 +1,14 @@
+package com.example.appsiot_lab2.Teclado;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TecladoNuevoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teclado_nuevo);
+    }
+}
