d6e8e52a0d9e346f16f8bdc9154eaeaa7a7ec81d
Leo <a20185896@pucp.edu.pe>
2022-09-17 18:41:03 -0500

    computadora actualizar

--- app/src/main/java/com/example/appsiot_lab2/Computadora/ComputadoraActualizarActivity.java
+++ app/src/main/java/com/example/appsiot_lab2/Computadora/ComputadoraActualizarActivity.java
@@ -6,11 +6,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.example.appsiot_lab2.Lab2Application;
 import com.example.appsiot_lab2.R;
 import com.example.appsiot_lab2.entity.Computadora;
+
+import java.util.ArrayList;
 
 public class ComputadoraActualizarActivity extends AppCompatActivity {
 
@@ -22,14 +28,24 @@ public class ComputadoraActualizarActivity extends AppCompatActivity {
         Intent intent = getIntent();
         Computadora computadora = (Computadora) intent.getSerializableExtra("computadora");
 
-        TextView textView = findViewById(R.id.textViewActivocomp);
+        TextView textView = findViewById(R.id.tv_activo_compu_act);
+        Spinner spinner = findViewById(R.id.sp_marca_compu_act);
+        EditText editText = findViewById(R.id.et_anio_compu_act);
+        EditText editText1 = findViewById(R.id.et_cpu_compu_act);
 
-        EditText editText = findViewById(R.id.plaintextAnioactcompu);
-        EditText editText1 = findViewById(R.id.plaintextCpuact);
+        String compareValue = computadora.getMarca();
 
         textView.setText(computadora.getActivo());
         editText.setText(computadora.getAnio());
         editText1.setText(computadora.getCpu());
+
+        int index = 0;
+        for(int i = 0; i < spinner.getCount(); i++){
+            if(spinner.getItemAtPosition(i).toString().equals(compareValue)){
+                spinner.setSelection(i);
+                break;
+            }
+        }
 
     }
 
@@ -39,6 +55,25 @@ public class ComputadoraActualizarActivity extends AppCompatActivity {
         return true;
     }
 
+    public void obtenidos (View view){
 
+        TextView textView = findViewById(R.id.tv_activo_compu_act);
+        EditText editText = findViewById(R.id.et_anio_compu_act);
+        EditText editText1 = findViewById(R.id.et_cpu_compu_act);
+
+        String activo = textView.getText().toString();
+        String anio = editText.getText().toString();
+        String cpu = editText1.getText().toString();
+
+        ArrayList<Computadora> computadoraList = ((Lab2Application) ComputadoraActualizarActivity.this.getApplication()).getComputadoraList();
+        for(Computadora computadora : computadoraList){
+            String activoobt = computadora.getActivo();
+            if(activo.equals(activoobt)){
+                computadora.setAnio(anio);
+                computadora.setCpu(cpu);
+            }
+
+        }
+    }
 
 }
